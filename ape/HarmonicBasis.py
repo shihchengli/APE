fix overflow errors appearing in solving Schrodinger equations

ape/HarmonicBasis.py
@@ -6,6 +6,11 @@ from scipy.special import erf
 from math import factorial as fact
 from math import pi, sqrt
 
+# fix overflow problem
+from decimal import Decimal as D
+from decimal import getcontext
+getcontext().prec = 15
+
 def Hermite(m,x):
     if m < 0:
         value = 0
@@ -24,38 +29,41 @@ def Hermite(m,x):
             P1 = value
             if i == m:
                 break
-    return value
+    return D(value)
 
-def IndefInt(m,n,x):
+def IndefInt(m,n,x,f):
+    f1 = f[0]
+    f2 = f[1]
+    f3 = f[2]
     x_max = np.sqrt(np.log(sys.float_info[0]))
-    value = 0
+    value = D(0)
     if x > x_max:
         if m == n:
-            value += pow(2.,n)*fact(n)*sqrt(pi)/2
+            value += D(pow(2.,n))/f1*D(fact(n))*D(sqrt(pi)/2)/f2/f3
             
     elif x < -x_max:
         if m == n:
-            value -= pow(2.,n)*fact(n)*sqrt(pi)/2
+            value -= D(pow(2.,n))/f1*D(fact(n))*D(sqrt(pi)/2)/f2/f3
 
     else:
         if m != n:
-            value = exp(-pow(x,2))*(n*Hermite(m,x)*Hermite(n-1,x)-m*Hermite(n,x)*Hermite(m-1,x))/(m-n)
+            value = D(exp(-pow(x,2)))*(D(n)*Hermite(m,x)/f1*Hermite(n-1,x)/f2-D(m)*Hermite(n,x)/f1*Hermite(m-1,x)/f2)/D(m-n)/f3
         else:
-            value += pow(2.,n)*sqrt(pi)/2*erf(x)
+            value += D(pow(2.,n)*sqrt(pi)/2)/f1*D(erf(x))/f2/f3
             for i in range(n):
-                value -= exp(-pow(x,2))*Hermite(i+1,x)*Hermite(i,x)*pow(2.,n-1-i)/fact(i+1)
-            value *= fact(n)
+                value -= D(exp(-pow(x,2)))*Hermite(i+1,x)/f1*Hermite(i,x)/f2*D(pow(2.,n-1-i))/D(fact(i+1))/f3
+            value *= D(fact(n))
     return value
 
-def IntHmHnexp(m,n,x1,x2):
-    return (IndefInt(m,n,x2)- IndefInt(m,n,x1))
+def IntHmHnexp(m,n,x1,x2,f):
+    return (IndefInt(m,n,x2,f)- IndefInt(m,n,x1,f))
 
-def IntXHmHnexp(m,n,x1,x2,a):
+def IntXHmHnexp(m,n,x1,x2,a,f):
     y=0
-    if (a[0]!=0): y += a[0]*IntHmHnexp(m,n,x1,x2)
-    if (a[1]!=0): y += a[1]*(1/2.0*IntHmHnexp(m,n+1,x1,x2)+n*IntHmHnexp(m,n-1,x1,x2))
-    if (a[2]!=0): y += a[2]*(1/4.0*IntHmHnexp(m,n+2,x1,x2)+(n+1/2.0)*IntHmHnexp(m,n,x1,x2)\
-            +n*(n-1)*IntHmHnexp(m,n-2,x1,x2))
-    if (a[3]!=0): y += a[3]*(1/8.0*IntHmHnexp(m,n+3,x1,x2)+3/4.0*(n+1)*IntHmHnexp(m,n+1,x1,x2)\
-            +3/2.0*n*n*IntHmHnexp(m,n-1,x1,x2) + n*(n-1)*(n-2)*IntHmHnexp(m,n-3,x1,x2))
+    if (a[0]!=0): y += D(a[0])*IntHmHnexp(m,n,x1,x2,f)
+    if (a[1]!=0): y += D(a[1])*(D(1/2.0)*IntHmHnexp(m,n+1,x1,x2,f)+D(n)*IntHmHnexp(m,n-1,x1,x2,f))
+    if (a[2]!=0): y += D(a[2])*(D(1/4.0)*IntHmHnexp(m,n+2,x1,x2,f)+D(n+1/2.0)*IntHmHnexp(m,n,x1,x2,f)\
+            +D(n*(n-1))*IntHmHnexp(m,n-2,x1,x2,f))
+    if (a[3]!=0): y += D(a[3])*(D(1/8.0)*IntHmHnexp(m,n+3,x1,x2,f)+D(3/4.0*(n+1))*IntHmHnexp(m,n+1,x1,x2,f)\
+            +D(3/2.0*n*n)*IntHmHnexp(m,n-1,x1,x2,f) + D(n*(n-1)*(n-2))*IntHmHnexp(m,n-3,x1,x2,f))
     return y

ape/schrodinger.py
@@ -9,6 +9,11 @@ import rmgpy.constants as constants
 from ape.HarmonicBasis import IntXHmHnexp
 from ape.FourierBasis import IntXPhimPhin
 
+# fix overflow problem
+from decimal import Decimal as D
+from decimal import getcontext
+getcontext().prec = 15
+
 hbar1 = constants.hbar / constants.E_h # in hartree*s
 hbar2 = constants.hbar * 10 ** 20 / constants.amu # in amu*angstrom^2/s
 def Hmn(m, n, polynomial_dict, mode_dict, energy_dict, mode, is_tors):
@@ -65,16 +70,22 @@ def Hmn(m, n, polynomial_dict, mode_dict, energy_dict, mode, is_tors):
                 x2 = delta_q*i/R
                 x1 = delta_q*(i-1)/R
             
+            f1 = D.sqrt(D(fact(m)))
+            f2 = D(pow(2,m/2.0)*pow(2,n/2.0)*sqrt(np.pi))
+            f3 = D.sqrt(D(fact(n)))
+            f = [f1,f2,f3]
+
             negative_energy = check_negative_energy(a,x1,x2)
             if negative_energy:
                 root1, root2 = negative_energy
-                result += IntXHmHnexp(m,n,x1,root1,a)
-                result += IntXHmHnexp(m,n,root2,x2,a)
+                result += IntXHmHnexp(m,n,x1,root1,a,f)
+                result += IntXHmHnexp(m,n,root2,x2,a,f)
             else:
-                result += IntXHmHnexp(m,n,x1,x2,a)
-        result /= pow(2,m/2.0)*pow(2,n/2.0)*sqrt(fact(m))*sqrt(fact(n))*sqrt(np.pi)
-        if m==n: result += -(1/2)*P*(2*m+1)
-        elif m == (n+2): result += sqrt(m)*sqrt(m-1)*(1/2)*P
+                result += IntXHmHnexp(m,n,x1,x2,a,f)
+
+        if m==n: result += D(-(1/2)*P*(2*m+1))
+        elif m == (n+2): result += D(sqrt(m)*sqrt(m-1)*(1/2)*P)
+
     return result
 
 def SetAnharmonicH(polynomial_dict, mode_dict, energy_dict, mode, size, N_prev, H_prev):